400bb02ab57483c3ea76fb5bd2017fda2ef4f7aa
Patrick Berry <patrickcberry@gmail.com>
2021-03-02 22:51:25 +1100

    General update

---
+++ pages/first.md
@@ -0,0 +1,10 @@
+title: A title
+date: 10/10/2021
+category: General
+# First blog post
+
+This is some text
+
+- List 1
+- List 2
+- List 3
--- site.py
+++ site.py
@@ -1,14 +1,26 @@
 import sys
 
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
 
 DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+pages = FlatPages(app)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/<path:path>.html')
+def page(path):
+    print('page function')
+    print(f'path: {path}')
+    page = pages.get_or_404(path)
+    return render_template('page.html',page=page)
 
 app.run(host='0.0.0.0', port=5001)
